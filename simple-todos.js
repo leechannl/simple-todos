0f16ca53b24cd68edcff7b54e9e2fb4e317771ba
leechannl <leechannl@gmail.com>
2015-04-28 23:38:46 +0800

    Update and delete todos.

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,36 @@
 // simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks:  [
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"},
-    ]
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(e, t){
+      var text = e.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      e.target.text.value = "";
+      return false;
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function(e, t){
+        Tasks.update({_id:this._id}, {$set:{
+          checked: !this.checked
+        }});
+    },
+    "click .delete": function () {
+      Tasks.remove({_id:this._id});
+    }
   });
 }
 
